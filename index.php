d05b5cb5d682e485e78b69f8fccf0990aea5220d
middlename-alexander <52368090+middlename-alexander@users.noreply.github.com>
2019-06-30 01:09:22 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php require_once('functions.php'); ?>
+
+<!doctype html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <title>Alex Crist | UI/UX and Front-End Developer</title>
+  <meta name="author" content="Alex Crist">
+  <meta name="description" content="Web Developer in Arlington, Texas">
+  <meta name="keywords" content="html,css,php,mysql,javascript,jquery,illustrator,photoshop,ui/ux,front end,web developer,portfolio,alex crist">  
+  
+  <link rel="stylesheet" href="css/html5boilerplate/normalize.css">
+  <link rel="stylesheet" href="css/html5boilerplate/main.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <!--  <link rel="stylesheet" media="screen and (min-width:1366px)" href="css/1366.css">-->
+  
+</head>
+<body>
+
+<header id="welcome">
+
+  <nav>
+    <ul>
+      <li><a href="#welcome">WELCOME</a></li>
+      <li><a href="#about">ABOUT</a></li>
+      <li><a href="#projects">PROJECTS</a></li>
+      <li><a href="#skills">SKILLS</a></li>
+      <li><a href="#contact">CONTACT</a></li>
+    </ul>
+  </nav>
+  
+  <div id="greeting">
+    <img id="headshot" src="img/headshot.png" alt="Photo of me"/>
+    <span id="hi-im">Hi, I'm</span>
+    <span id="alex">Alex</span>
+    <span id="one-liner">UI/UX Designer and Front-End Developer from Arlington, Texas.</span>
+    <span id="hand-coded">This website is hand-coded in plain HTML, CSS, PHP, and JavaScript<br />with focus on responsive design and cross-browser compatibility.</span>
+    <br />
+    <a class="btn btn-white" href="resume.pdf"><img src="img/download-icon.svg" alt="download icon">DOWNLOAD RESUME</a>
+  </div>
+  
+</header>
+  
+<section id="about">
+
+  <div id="about-left">
+    <h1>My story so far</h1>
+    <p>I see the web as a wonder of human achievement in our process of becoming connected with others. My enthusiasm for computers started early and I had programmed games on my TI-83 graphing calculator in middle school. By highschool, I was reading C++ books but was given a book on HTML and started making websites.</p>
+    <p>Now I am fortunate to specialize in making beautiful, usable, and professional websites for others using best practices and industry standards. My <a class="body-text-link" href="#skills">tools of creation</a> include HTML, CSS, PHP, JavaScript, MySQL, Wordpress, and Photoshop.</p>
+    <p>To clear my cache, I visit nature and exercise on a nature trail with work out stations called Timberform Fitness.</p>
+  </div>
+  
+  <div id="about-right">
+
+  </div>
+  
+</section>
+
+<section id="projects">
+
+  <h1>Projects of mine</h1>
+  <ul><?php makeProjects(); ?></ul>
+
+</section>
+
+<section id="skills">
+
+  <h1>Tools of creation</h1>
+  <ul><?php makeSkills(); ?></ul>
+
+</section>
+
+<footer id="contact">
+  <!--<p>
+    <div style="width:90%;">Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+  </p>-->
+</footer>
+</body>
+</html>
